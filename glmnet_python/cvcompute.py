- calculation of CVmetrics for cvlognet are corrected
 - importantly if a CV-fold does not converge for a given lambda Inf predictions instead of NaN are returned so that
 - those lambdas are not considered for lambda_min or lambda_1se considerations... in some CV folds cases with unconverged lambda's
   cvsd got Inf and then lambda_1se was returning the highest lambda => 0 feature model

glmnet_python/cvcompute.py
@@ -4,31 +4,36 @@ Internal glmnet function. See also cvglmnet.
 
 Compute the weighted mean and SD within folds, and hence the SE of the mean
 """
-import scipy
-from wtmean import wtmean
+import numpy as np
+from glmnet_python.wtmean import wtmean
+
 
 def cvcompute(mat, weights, foldid, nlams):
+    # remove skipped samples (foldid is negative) from considerations
+    mat = mat[foldid >= 0]
+    weights = weights[foldid >= 0]
+    foldid = foldid[foldid >= 0]
+
     if len(weights.shape) > 1:
-        weights = scipy.reshape(weights, [weights.shape[0], ])
-    wisum = scipy.bincount(foldid, weights = weights)
-    nfolds = scipy.amax(foldid) + 1
-    outmat = scipy.ones([nfolds, mat.shape[1]])*scipy.NaN
-    good = scipy.zeros([nfolds, mat.shape[1]])
-    mat[scipy.isinf(mat)] = scipy.NaN
+        weights = np.reshape(weights, [weights.shape[0], ])
+    wisum = np.bincount(foldid, weights=weights)
+    nfolds = np.amax(foldid) + 1
+    outmat = np.ones([nfolds, mat.shape[1]]) * np.NaN
+    good = np.zeros([nfolds, mat.shape[1]])
+    #mat[np.isinf(mat)] = np.NaN
     for i in range(nfolds):
         tf = foldid == i
         mati = mat[tf, ]
         wi = weights[tf, ]
         outmat[i, :] = wtmean(mati, wi)
         good[i, 0:nlams[i]] = 1
-    N = scipy.sum(good, axis = 0)
+    N = np.sum(good, axis=0)
     cvcpt = dict()
     cvcpt['cvraw'] = outmat
     cvcpt['weights'] = wisum
     cvcpt['N'] = N
 
-    return(cvcpt)
+    return cvcpt
 
 # end of cvcompute
-#=========================    
-    
+# =========================

glmnet_python/cvglmnet.py
@@ -197,15 +197,15 @@ OUTPUT ARGUMENTS:
 import sys
 import joblib
 import multiprocessing
-from glmnetSet import glmnetSet
-from glmnetPredict import glmnetPredict
+from glmnet_python.glmnetSet import glmnetSet
+from glmnet_python.glmnetPredict import glmnetPredict
 import scipy
-from glmnet import glmnet
-from cvelnet import cvelnet
-from cvlognet import cvlognet
-from cvmultnet import cvmultnet
-from cvmrelnet import cvmrelnet
-from cvfishnet import cvfishnet
+from glmnet_python.glmnet import glmnet
+from glmnet_python.cvelnet import cvelnet
+from glmnet_python.cvlognet import cvlognet
+from glmnet_python.cvmultnet import cvmultnet
+from glmnet_python.cvmrelnet import cvmrelnet
+from glmnet_python.cvfishnet import cvfishnet
 
 def cvglmnet(*, x,
              y,
@@ -287,25 +287,25 @@ def cvglmnet(*, x,
             cpredmat.append(newFit)
         
     if cpredmat[0]['class'] == 'elnet':
-        cvstuff = cvelnet( cpredmat, options['lambdau'], x, y \
-                          , options['weights'], options['offset'] \
-                          , foldid, ptype, grouped, keep)
+        cvstuff = cvelnet( cpredmat, options['lambdau'], x, y,
+                          options['weights'], options['offset'],
+                          foldid, ptype, grouped, keep)
     elif cpredmat[0]['class'] == 'lognet':
-        cvstuff = cvlognet(cpredmat, options['lambdau'], x, y \
-                          , options['weights'], options['offset'] \
-                          , foldid, ptype, grouped, keep)
+        cvstuff = cvlognet(cpredmat, options['lambdau'], x, y,
+                           options['weights'], options['offset'],
+                           foldid, nfolds, ptype, grouped, keep)
     elif cpredmat[0]['class'] == 'multnet':
-        cvstuff = cvmultnet(cpredmat, options['lambdau'], x, y \
-                          , options['weights'], options['offset'] \
-                          , foldid, ptype, grouped, keep)
+        cvstuff = cvmultnet(cpredmat, options['lambdau'], x, y,
+                           options['weights'], options['offset'],
+                           foldid, ptype, grouped, keep)
     elif cpredmat[0]['class'] == 'mrelnet':
-        cvstuff = cvmrelnet(cpredmat, options['lambdau'], x, y \
-                          , options['weights'], options['offset'] \
-                          , foldid, ptype, grouped, keep)
+        cvstuff = cvmrelnet(cpredmat, options['lambdau'], x, y,
+                            options['weights'], options['offset'],
+                            foldid, ptype, grouped, keep)
     elif cpredmat[0]['class'] == 'fishnet':
-        cvstuff = cvfishnet(cpredmat, options['lambdau'], x, y \
-                           , options['weights'], options['offset'] \
-                           , foldid, ptype, grouped, keep)
+        cvstuff = cvfishnet(cpredmat, options['lambdau'], x, y,
+                            options['weights'], options['offset'],
+                            foldid, ptype, grouped, keep)
     elif cpredmat[0]['class'] == 'coxnet':
         raise NotImplementedError('Cross-validation for coxnet not implemented yet.')
         #cvstuff = cvcoxnet(cpredmat, options['lambdau'], x, y \
@@ -336,12 +336,12 @@ def cvglmnet(*, x,
     CVerr['lambda_1se'] = scipy.amax(options['lambdau'][cvm <= semin]).reshape([1])
     CVerr['class'] = 'cvglmnet'
     
-    return(CVerr)
+    return CVerr
         
 # end of cvglmnet
 #==========================
 def doCV(i, x, y, family, foldid, nfolds, is_offset, **options):
-    which = (foldid == i) | (foldid>=nfolds)
+    which = (foldid == i) | (foldid<0)
     opts = options.copy()
     opts['weights'] = opts['weights'][~which, ]
     opts['lambdau'] = options['lambdau']
@@ -351,5 +351,5 @@ def doCV(i, x, y, family, foldid, nfolds, is_offset, **options):
     xr = x[~which, ]
     yr = y[~which, ]
     newFit = glmnet(x = xr, y = yr, family = family, **opts)    
-    return(newFit)
+    return newFit
     

glmnet_python/cvlognet.py
@@ -4,19 +4,20 @@ Internal function called by cvglmnet. See also cvglmnet
 
 """
 import scipy
-from glmnetPredict import glmnetPredict
-from wtmean import wtmean
-from cvcompute import cvcompute
+from glmnet_python.glmnetPredict import glmnetPredict
+from glmnet_python.wtmean import wtmean
+from glmnet_python.cvcompute import cvcompute
 
-def cvlognet(fit, \
-            lambdau, \
-            x, \
-            y, \
-            weights, \
-            offset, \
-            foldid, \
-            ptype, \
-            grouped, \
+def cvlognet(fit,
+            lambdau,
+            x,
+            y,
+            weights,
+            offset,
+            foldid,
+            nfolds,
+            ptype,
+            grouped,
             keep = False):
     
     typenames = {'deviance':'Binomial Deviance', 'mse':'Mean-Squared Error', 
@@ -37,11 +38,7 @@ def cvlognet(fit, \
         nc = len(classes)
         indexes = scipy.eye(nc, nc)
         y = indexes[sy, :]
-    else:
-        classes = scipy.arange(nc) + 1 # 1:nc
-        
     N = y.size
-    nfolds = scipy.amax(foldid) + 1
     if (N/nfolds < 10) and (type == 'auc'):
         print('Warning: Too few (<10) observations per fold for type.measure=auc in cvlognet')
         print('Warning:     changed to type.measure = deviance. Alternately, use smaller value ')
@@ -53,9 +50,8 @@ def cvlognet(fit, \
         grouped = False
 
     is_offset = not(len(offset) == 0)
-    predmat = scipy.ones([y.shape[0], lambdau.size])*scipy.NAN               
-    nfolds = scipy.amax(foldid) + 1
-    nlams = []    
+    predmat = scipy.ones([y.shape[0], lambdau.size])*scipy.Inf
+    nlams = []
     for i in range(nfolds):
         which = foldid == i
         fitobj = fit[i].copy()
@@ -68,7 +64,7 @@ def cvlognet(fit, \
         predmat[which, 0:nlami] = preds
         nlams.append(nlami)
     # convert nlams to scipy array
-    nlams = scipy.array(nlams, dtype = scipy.integer)
+    nlams = scipy.array(nlams, dtype=scipy.integer)
 
     if ptype == 'auc':
         cvraw = scipy.zeros([nfolds, lambdau.size])*scipy.NaN
@@ -90,9 +86,8 @@ def cvlognet(fit, \
         N = y.shape[0] - scipy.sum(scipy.isnan(predmat), axis = 0, keepdims = True)
         yy1 = scipy.tile(y[:,0:1], [1, lambdau.size])
         yy2 = scipy.tile(y[:,1:2], [1, lambdau.size])
-
     if ptype == 'mse':
-        cvraw = (yy1 - (1 - predmat))**2 + (yy2 - (1 - predmat))**2
+        cvraw = (yy1 - (1 - predmat))**2
     elif ptype == 'deviance':
         predmat = scipy.minimum(scipy.maximum(predmat, prob_min), prob_max)
         lp = yy1*scipy.log(1-predmat) + yy2*scipy.log(predmat)
@@ -101,7 +96,7 @@ def cvlognet(fit, \
         ly = scipy.dot(y*ly, scipy.array([1.0, 1.0]).reshape([2,1]))
         cvraw = 2*(scipy.tile(ly, [1, lambdau.size]) - lp)
     elif ptype == 'mae':
-        cvraw = scipy.absolute(yy1 - (1 - predmat)) + scipy.absolute(yy2 - (1 - predmat))
+        cvraw = scipy.absolute(yy1 - (1 - predmat))
     elif ptype == 'class':
         cvraw = yy1*(predmat > 0.5) + yy2*(predmat <= 0.5)
     
@@ -114,7 +109,6 @@ def cvlognet(fit, \
         cvraw = cvob['cvraw']
         weights = cvob['weights']
         N = cvob['N']
-        
     cvm = wtmean(cvraw, weights)
     sqccv = (cvraw - cvm)**2
     cvsd = scipy.sqrt(wtmean(sqccv, weights)/(N-1))